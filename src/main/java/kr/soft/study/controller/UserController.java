e70cdd7fff09b562a565667948708ab47cb8163e
BANSUHEE <162074625+BANSUHEE@users.noreply.github.com>
2024-07-01 19:33:34 +0900

    07.01 암호화까지 완성

--- src/main/java/kr/soft/study/controller/UserController.java
+++ src/main/java/kr/soft/study/controller/UserController.java
@@ -1,113 +1,101 @@
 package kr.soft.study.controller;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import java.net.http.HttpRequest;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import kr.soft.study.command.user.Join;
-import kr.soft.study.command.user.UserCommand;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-import kr.soft.study.command.user.Login;
+//import kr.soft.study.command.user.Join;
 import kr.soft.study.command.user.UserCommand;
+import kr.soft.study.command.user.Login;
+import kr.soft.study.command.user.Join;
 
 @Controller
 public class UserController {
-	
-	UserCommand command=null;
-	
-	private final UserCommand Join;
-	
+
+	private UserCommand command;
+
 	@Autowired
 	private SqlSession sqlSession;
-	
-	@Autowired
-	public UserController(Join join) {
-		this.Join = join;
-	}
 
-			
-	@RequestMapping(value="/Join", method=RequestMethod.POST)
-	public String join(Model model,HttpServletRequest request) {
-		System.out.println("join()");
-		
-		model.addAttribute("request",request);
-		Join.execute(model);
-		
-		return "loginpage";
-	}
-	
-	
 	// home
 	@RequestMapping("/")
 	public String main(Model model) {
-		
+
 		return "main";
 	}
-	
-	
 
-	//home리다이렉트용
+	// home리다이렉트용
 	@RequestMapping("/main")
 	public String mainRedirect(Model model) {
 		System.out.println("main()");
-		
+
 		return "main";
 	}
-	
-	
+
 	// 로그인화면이동
 	@RequestMapping("/loginView")
 	public String loginView(Model model) {
-		
+
 		System.out.println("loginView()");
-		
+
 		return "user/login";
 	}
-	//로그인
+
+	// 로그인
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session,HttpServletRequest request,Model model) {
-		
+	public String login(HttpSession session, HttpServletRequest request, Model model) {
+
 		System.out.println("login()");
-        model.addAttribute("request", request);
-   
-		command=new Login(sqlSession);
+		model.addAttribute("request", request);
+
+		command = new Login(sqlSession);
 		command.execute(model);
-		String path=(String) model.asMap().get("path");
-		String email=(String) model.asMap().get("email");
+		String path = (String) model.asMap().get("path");
+		String email = (String) model.asMap().get("email");
 
 		session.setAttribute("email", email);
 		return path;
 	}
-	
+
+	// 회원가입
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	public String join(@RequestParam("name") String name,  HttpSession Session, HttpServletRequest request, Model model) {
+
+		System.out.println("join() 메서드 시작");
+		model.addAttribute("request", request);
+//		model.addAttribute("name", name);
+		System.out.println("name :" + name);
+		command = new Join(sqlSession);
+		command.execute(model);
+		String path = (String) model.asMap().get("path");
+		System.out.println("path :" + path);
+
+		return path;
+	}
+
 	// 로그아웃
 	@RequestMapping("/logout")
-    public String logout(HttpServletRequest request) {
+	public String logout(HttpServletRequest request) {
 
-        System.out.println("logout()");
-        // 현재 세션 객체를 가져옴
-        HttpSession session = request.getSession();
-        // 세션 무효화 
-        session.invalidate();
-        return "redirect:/main";
-    }
-	
-	// 회원가입
-    @RequestMapping("/join")
-    public String join(Model model) {
-        System.out.println("join()");
-        return "user/join";
-    }
+		System.out.println("logout()");
+		// 현재 세션 객체를 가져옴
+		HttpSession session = request.getSession();
+		// 세션 무효화
+		session.invalidate();
+		return "redirect:/main";
+	}
+
+	// 회원가입 화면 이동
+	@RequestMapping("/joinView")
+	public String joinView(Model model) {
+		System.out.println("joinView()");
+		return "user/join";
+	}
 
 }
--- src/main/java/kr/soft/study/util/UserDao.java
+++ src/main/java/kr/soft/study/util/UserDao.java
@@ -1,8 +1,5 @@
 package kr.soft.study.util;
 
-
-import javax.servlet.http.HttpSession;
-
 import kr.soft.study.dto.Users;
 
 public interface UserDao {
